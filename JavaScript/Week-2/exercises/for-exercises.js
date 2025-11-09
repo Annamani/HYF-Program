440b11c1ab46ea8677ddd64d2362716a5d00bacb
annamani <annamanik.vit@gmail.com>
2025-11-09 22:12:47 +0100

    for -loop smal exercises

---
+++ JavaScript/Week-2/exercises/for-exercises.js
@@ -0,0 +1,12 @@
+// Create a for loop that logs out the numbers from 74 - 98
+// for (let i = 74; i <= 98; i++) {
+//     console.log(i);
+// }
+
+function logString(stringToLog, numberOfTimesToLog) {
+    for (let i = 1; i <= numberOfTimesToLog; i++) {
+        console.log(stringToLog);
+    }
+}
+logString("hello", 3);
+logString("Annamani", 5);
