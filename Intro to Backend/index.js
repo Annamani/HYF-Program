5b68d8cfa2053f1ebf7b741e2e1cee2f11d09058
annamani <annamanik.vit@gmail.com>
2025-12-14 13:45:04 +0100

    get users logic updated

--- Intro to Backend/index.js
+++ Intro to Backend/index.js
@@ -7,7 +7,9 @@ app.use(express.json());
 app.get("/", (req, res) => {
     res.send("Hello, Annamani!");
 });
-
+app.get("/users", (req, res) => {
+    res.send("Users List is ready");
+});
 app.listen(PORT, () => {
     console.log(`Server is running on  http://localhost:${PORT}`);
 });
